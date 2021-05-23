640ca8993aa29f312f2c9282077cd326c563bb8b
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2021-05-23 16:08:02 +1000

    add html and exit example

--- sandbox/node-server-basic/index.js
+++ sandbox/node-server-basic/index.js
@@ -3,6 +3,18 @@ const http = require("http")
 function requestListener(req, res) {
   req.on("end", () => console.log("Server end"))
 
+  if (req.url === "/exit") {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.end("Sorry but I'm about to exit")
+    return process.exit()
+  }
+
+  if (req.url === "/html") {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(`<h1>HTML response example</h1>`)
+    return res.end()
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('okay')
 }
